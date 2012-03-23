5514ec2e41398d5289758c896afef4b21621916d
unknown <chesnokov@WSM6-0037.netcracker.com>
2012-03-23 17:10:18 +0400

    Simple servlet sample added

---
+++ simple-servlet/src/main/java/com/wordpress/dev64/servlet/simple/SimpleServlet.java
@@ -0,0 +1,31 @@
+package com.wordpress.dev64.servlet.simple;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SimpleServlet extends HttpServlet {
+
+    private String responseTemplate =
+            "<html>\n" +
+                    "<body>\n" +
+                    "<h2>Hello from Simple Servlet</h2>\n" +
+                    "</body>\n" +
+                    "</html>";
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        this.process(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        this.process(request, response);
+    }
+
+    private void process(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setStatus(200);
+        response.getWriter().write(responseTemplate);
+    }
+}
